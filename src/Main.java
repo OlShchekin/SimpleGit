1084ee16e6e40329ce02fd598abb1dfb3d91e9d5
Shchekin Oleg <olegshchekin09@gmail.com>
2019-04-25 22:04:00 +0200

    new class

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
+import com.Action;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Git");
+        Action A = new Action(20, "Lis");
+        System.out.println(A.toString());
     }
 }
